04016c2977099574e10493520d0427f4597e3730
Luke Collins <collins.luke@gmail.com>
2025-08-25 19:04:45 +0000

    Updates

--- containers/app/build.gradle.kts
+++ containers/app/build.gradle.kts
@@ -19,25 +19,29 @@ details.isCleanTag
 val containerImageName = "ghcr.io/lcollins/hello-world-app"
 val containerImageTag = "${details.gitHash}"
 
-tasks.register<DockerBuildImage>("buildDockerImage") {
+tasks.register<DockerBuildImage>("dockerTagImage") {
     inputDir.set(file(project.projectDir)) // Use the project root as the Docker context
-    images.add("$containerImageName:$containerImageTag") // Correctly specify the image tags
+    images.addAll(
+        "$containerImageName:$containerImageTag",
+        "$containerImageName:${details.branchName}-latest",
+        "$containerImageName:stable")
     pull.set(true) // Ensure the base image is pulled
     noCache.set(false)
 }
 
-tasks.register<DockerTagImage>("dockerTagImage") {
-    imageId.set("$containerImageName:$containerImageTag")
-    repository.set(containerImageName)
-    tag.set("latest")
-    dependsOn("buildDockerImage")
-}
+//tasks.register<DockerTagImage>("dockerTagImage") {
+//    imageId.set("$containerImageName:$containerImageTag")
+//    repository.set(containerImageName)
+//    tag.set("${details.branchName}-latest")
+//    dependsOn("buildDockerImage")
+//}
 
 tasks.register<DockerPushImage>("dockerPushImage") {
     images.addAll(
         listOf(
             "$containerImageName:$containerImageTag",
-            "$containerImageName:latest"
+            "$containerImageName:${details.branchName}-latest",
+            "$containerImageName:stable"
         )
     )
     dependsOn("dockerTagImage")
